2e9888d1d7ea2a098e6ed276aec388733a5d0cf6
LifeBringer <ramiz.alda@gmail.com>
2021-03-21 22:55:12 -0400

    initial commit

---
+++ RGram/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  RGram
+//
+//  Created by R Alda on 2021-03-21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
