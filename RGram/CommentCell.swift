184e1bab1624b41644d15883f43aadf9671c0813
LifeBringer <ramiz.alda@gmail.com>
2021-03-23 22:54:22 -0400

    Part2 Complete

---
+++ RGram/CommentCell.swift
@@ -0,0 +1,23 @@
+//
+//  CommentCell.swift
+//  RGram
+//
+//  Created by R Alda on 2021-03-23.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
